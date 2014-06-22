9c9c721d9aa57dd3692e4442e52147e737136071
wiadufachen <wiadufachen@gmail.com>
2014-06-22 22:22:05 +0800

    Add travis

--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
+try:
+    import setuptools
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    
 from setuptools import setup, Extension
-from subprocess import call
-import sys
-swig = "h:\\Python\\swigwin-3.0.2\\swig.exe"
-
-if sys.version < '3':
-    def smart_input(x):
-        return raw_input(x)
-else:
-    def smart_input(x):
-        return input(x)
-temp = smart_input("Where is swig?[%s]:" % swig)
-if len(temp) > 0:
-    swig = temp
-call([swig, "-python", "-c++", "pytinyxml2.i"])
 
 
 files = ["tinyxml2.cpp", "pytinyxml2_wrap.cxx"]
@@ -25,7 +17,7 @@ setup(name="pytinyxml2",
       url="https://github.com/wiadufachen/pytinyxml2",
       py_modules=['pytinyxml2'],
       ext_modules=[
-                    Extension("_pytinyxml2", files),
+                    Extension("_pytinyxml2", sources=files)
                     ],
       classifiers=["Programming Language :: Python",
              "Programming Language :: Python :: 3",
---
+++ update.bat
@@ -0,0 +1,2 @@
+h:\Python\swigwin-3.0.2\swig -python -c++ pytinyxml2.i
+pause
